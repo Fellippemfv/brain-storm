abc2245c4e5bcb5d8229ba528473a46672140060
vitorhfv <vitorhfv2@gmail.com>
2023-07-08 18:39:27 -0400

    update files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 import React, { useState } from 'react';
 import Draggable from 'react-draggable';
 import styles from './page.module.css';
+import SteppedLineTo from 'react-lineto';
 
 interface Div {
+    id: string;
     position: {
         x: number;
         y: number;
@@ -14,41 +16,70 @@ interface Div {
 
 export default function Home() {
     const [divs, setDivs] = useState<Div[]>([]);
-    const [mainText, setMainText] = useState('');
-    const [description, setDescription] = useState('');
+    const [connections, setConnections] = useState<string[]>([]);
+    const [selectedDivIds, setSelectedDivIds] = useState<string[]>([]);
 
-    const handleDrag = (e: any, ui: any, index: number) => {
-        const { x, y } = divs[index].position;
-        const newDivs = [...divs];
-        newDivs[index] = {
-            ...newDivs[index],
-            position: { x: x + ui.deltaX, y: y + ui.deltaY },
-        };
-        setDivs(newDivs);
+    const handleDrag = (e: any, ui: any, id: string) => {
+        const index = divs.findIndex((div) => div.id === id);
+        if (index !== -1) {
+            const { x, y } = divs[index].position;
+            const newDivs = [...divs];
+            newDivs[index] = {
+                ...newDivs[index],
+                position: { x: x + ui.deltaX, y: y + ui.deltaY },
+            };
+            setDivs(newDivs);
+        }
     };
 
-    const handleMainTextChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-        const newDivs = [...divs];
-        newDivs[index] = {
-            ...newDivs[index],
-            mainText: e.target.value,
-        };
-        setDivs(newDivs);
+    const handleMainTextChange = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+        const index = divs.findIndex((div) => div.id === id);
+        if (index !== -1) {
+            const newDivs = [...divs];
+            newDivs[index] = {
+                ...newDivs[index],
+                mainText: e.target.value,
+            };
+            setDivs(newDivs);
+        }
     };
 
-    const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-        const newDivs = [...divs];
-        newDivs[index] = {
-            ...newDivs[index],
-            description: e.target.value,
-        };
-        setDivs(newDivs);
+    const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+        const index = divs.findIndex((div) => div.id === id);
+        if (index !== -1) {
+            const newDivs = [...divs];
+            newDivs[index] = {
+                ...newDivs[index],
+                description: e.target.value,
+            };
+            setDivs(newDivs);
+        }
     };
+
+    const handleCircleClick = (id: string) => {
+        if (selectedDivIds.includes(id)) {
+            const newSelectedDivIds = selectedDivIds.filter((divId) => divId !== id);
+            setSelectedDivIds(newSelectedDivIds);
+        } else {
+            if (selectedDivIds.length === 1) {
+                const [sourceId] = selectedDivIds;
+                const targetId = id;
+                const connection = `${sourceId}-${targetId}`;
+                setConnections([...connections, connection]);
+                setSelectedDivIds([]);
+            } else {
+                setSelectedDivIds([id]);
+            }
+        }
+    };
+
 
     const createThought = () => {
         const newDivs = [...divs];
+        const id = `div${newDivs.length}`;
         const position = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
         newDivs.push({
+            id,
             position,
             mainText: '',
             description: '',
@@ -56,44 +87,93 @@ export default function Home() {
         setDivs(newDivs);
     };
 
+
+
     return (
         <main className={styles.mainContainer}>
             <div className={styles.createButtonContainer}>
                 <button className={styles.createButton} onClick={createThought}>
                     Criar pensamento
                 </button>
+
             </div>
-            {divs.map((div, divIndex) => (
+            {divs.map((div) => (
                 <Draggable
-                    key={divIndex}
+                    key={div.id}
                     handle=".drag-handle"
-                    onDrag={(e, ui) => handleDrag(e, ui, divIndex)}
+                    onDrag={(e, ui) => handleDrag(e, ui, div.id)}
                 >
                     <div
-                        className={styles.draggableDiv}
+                        className={`${styles.draggableDiv} ${div.id}`}
                         style={{ left: div.position.x, top: div.position.y }}
+                        id={div.id}
                     >
                         <div className={`${styles.textareaContainer} drag-handle`}>
                             <input
                                 type="text"
                                 value={div.mainText}
-                                onChange={(e) => handleMainTextChange(e, divIndex)}
+                                onChange={(e) => handleMainTextChange(e, div.id)}
                                 placeholder="Texto principal"
                             />
                             <input
                                 type="text"
                                 value={div.description}
-                                onChange={(e) => handleDescriptionChange(e, divIndex)}
+                                onChange={(e) => handleDescriptionChange(e, div.id)}
                                 placeholder="Descrição"
                             />
                         </div>
-                        <div className={styles.circleTop} />
-                        <div className={styles.circleBottom} />
-                        <div className={styles.circleLeft} />
-                        <div className={styles.circleRight} />
+                        <div
+                            className={styles.circleTop}
+                            onClick={() => handleCircleClick(div.id)}
+                            style={{
+                                backgroundColor: selectedDivIds.includes(div.id) ? 'darkblue' : 'lightblue',
+                            }}
+                        />
+                        <div
+                            className={styles.circleBottom}
+                            onClick={() => handleCircleClick(div.id)}
+                            style={{
+                                backgroundColor: selectedDivIds.includes(div.id) ? 'darkblue' : 'lightblue',
+                            }}
+                        />
+                        <div
+                            className={styles.circleLeft}
+                            onClick={() => handleCircleClick(div.id)}
+                            style={{
+                                backgroundColor: selectedDivIds.includes(div.id) ? 'darkblue' : 'lightblue',
+                            }}
+                        />
+                        <div
+                            className={styles.circleRight}
+                            onClick={() => handleCircleClick(div.id)}
+                            style={{
+                                backgroundColor: selectedDivIds.includes(div.id) ? 'darkblue' : 'lightblue',
+                            }}
+                        />
                     </div>
                 </Draggable>
             ))}
+            <div className={styles.connectionContainer}>
+                {connections.map((connection) => {
+                    const [sourceId, targetId] = connection.split('-');
+                    const sourceDiv = divs.find((div) => div.id === sourceId);
+                    const targetDiv = divs.find((div) => div.id === targetId);
+                    if (sourceDiv && targetDiv) {
+                        return (
+                            <SteppedLineTo
+                                key={connection}
+                                from={sourceDiv.id}
+                                to={targetDiv.id}
+                                borderColor="skyblue"
+                                borderWidth={4}
+                                zIndex={-1}
+
+                            />
+                        );
+                    }
+                    return null;
+                })}
+            </div>
         </main>
     );
 }
